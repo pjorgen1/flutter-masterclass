c12e3575400ca49428bbffe76161969cf45cb016
Pierce Jorgensen <jorgensenpierce@gmail.com>
2026-01-10 21:37:43 -0500

    Finished Secions 4 and 5

---
+++ fmc_part_4/android/app/src/main/kotlin/com/example/fmc_part_4/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.fmc_part_4
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ fmc_part_4/lib/screens/profile/profile.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+import 'package:fmc_part_4/models/character.dart';
+import 'package:fmc_part_4/screens/profile/skill_list.dart';
+import 'package:fmc_part_4/screens/profile/stats_table.dart';
+import 'package:fmc_part_4/shared/styled_button.dart';
+import 'package:fmc_part_4/shared/styled_text.dart';
+import 'package:fmc_part_4/theme.dart';
+
+class Profile extends StatelessWidget {
+  const Profile({
+    super.key,
+    required this.character,
+  });
+
+  final Character character;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: StyledTitle(character.name),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+
+            // basic info
+            Container(
+              padding: const EdgeInsets.all(16),
+              color: AppColors.tertiaryColor,
+              child: Row(
+                children: [
+                  Image.asset('assets/img/vocations/${character.vocation.image}',
+                    width: 140,
+                    height: 140,
+                  ),
+                  const SizedBox(width: 20),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        StyledHeading(character.vocation.title),
+                        StyledText(character.vocation.description),
+                      ]
+                    ),
+                  )
+                ]
+              )
+            ),
+
+            // weapon and ability
+            const SizedBox(height: 20),
+            Center(child: Icon(Icons.rocket_launch, color: AppColors.primaryColor)),
+
+            Padding(
+              padding: const EdgeInsets.all(16),
+              child: Container(
+                width: double.infinity,
+                padding: const EdgeInsets.all(16),
+                color: AppColors.tertiaryColor,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const StyledHeading("Slogan"),
+                    StyledText(character.slogan),
+                    const SizedBox(height: 10),
+                    const StyledHeading("Weapon of Choice"),
+                    StyledText(character.vocation.weapon),
+                    const SizedBox(height: 10),
+                    const StyledHeading("Unique Ability"),
+                    StyledText(character.vocation.ability),
+                    const SizedBox(height: 10),
+                  ]
+                )
+              ),
+            ),
+
+            // stats and skills
+            Container(
+              alignment: Alignment.center,
+              child: Column(
+                children: [
+                  StatsTable(character),
+                  SkillList(character),
+                ]
+              ),
+            ),
+
+            // save button
+            StyledButton(
+              onPressed: () {
+                ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                  content: const StyledHeading("Character saved!"),
+                  showCloseIcon: true,
+                  duration: const Duration(seconds: 2),
+                  backgroundColor: AppColors.secondaryColor,
+                ));
+              },
+              child: const StyledHeading("Save Character"),
+            )
+
+          ]
+        )
+      )
+    );
+  }
+}
